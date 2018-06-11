840dda93ba1a50bb89efa39c65395824522d361e
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 04:15:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length <= 2
+  elsif array.length == 3
     return array.join(", and")
   end
 end
