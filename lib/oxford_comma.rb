21c308eb889455bc203b25579f60c721c319dcf7
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 04:23:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    for i in array
-      print i
-    end
+    array.join(", and")
   end
 end
 
